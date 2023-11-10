33a4bbe91422ec082e1037be6d8aafd13fa3079c
allistairyu <allistair_yu@brown.edu>
2023-11-10 17:40:43 +0000

    repl

---
+++ pkg/repl/repl.go
@@ -0,0 +1,176 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"iptcp/pkg/node"
+	"iptcp/pkg/tcpstack"
+	"net/netip"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func listenAndAccept(port uint16, t *tcpstack.TCPStack, n *node.Node) error {
+	lsocket, err := t.VListen(port)
+	if err != nil {
+		return err
+	}
+	for {
+		normalSock, err := lsocket.VAccept(t, n)
+		if err != nil {
+			return err
+		}
+		go normalSock.ReceiverThread(n)
+		go normalSock.SenderThread(n)
+	}
+}
+
+func REPL(node *node.Node, t *tcpstack.TCPStack) {
+	reader := bufio.NewScanner(os.Stdin)
+	fmt.Print("> ")
+	for reader.Scan() {
+		command := cleanInput(reader.Text())
+
+		tokens := strings.Split(command, " ")
+		switch tokens[0] {
+		case "li":
+			node.PrintInterfaces()
+		case "ln":
+			node.PrintNeighbors()
+		case "lr":
+			node.PrintRoutes()
+		case "down":
+			if len(tokens) != 2 {
+				fmt.Println("down usage: down <ifname>")
+			} else {
+				err := node.ToggleInterface(tokens[1], false)
+				if err != nil {
+					fmt.Println("Invalid interface")
+				}
+			}
+		case "up":
+			if len(tokens) != 2 {
+				fmt.Println("up usage: up <ifname>")
+			} else {
+				err := node.ToggleInterface(tokens[1], true)
+				if err != nil {
+					fmt.Println("Invalid interface")
+				}
+			}
+		case "send":
+			if len(tokens) < 3 {
+				fmt.Println("send usage: send <vip> <message ...>")
+			} else {
+				parsed_addr := netip.MustParseAddr(tokens[1])
+				_, _, found := node.FindNext(parsed_addr) // where to forward to; our "source"
+				if !found {
+					fmt.Println("Error: No match in forwarding table.")
+				} else {
+					node.HandleSend(parsed_addr, []byte(strings.Join(tokens[2:], " ")), 0)
+				}
+			}
+		case "a":
+			if len(tokens) != 2 {
+				fmt.Println("a usage: a <port>")
+			} else {
+				port, err := strconv.ParseUint(tokens[1], 10, 16)
+				if err != nil {
+					fmt.Println("Could not parse port as int")
+				}
+				go listenAndAccept(uint16(port), t, node)
+			}
+		case "c":
+			if len(tokens) != 3 {
+				fmt.Println("c usage: c <vip> <port>")
+			} else {
+				port, err := strconv.ParseUint(tokens[2], 10, 16)
+				if err != nil {
+					fmt.Println("Could not parse port as int")
+					continue
+				}
+				addr := netip.MustParseAddr(tokens[1])
+				if err != nil {
+					fmt.Println("Could not parse address")
+					continue
+				}
+				norm, err := t.VConnect(addr, uint16(port), node)
+				if err != nil {
+					fmt.Printf("Error: %s\n", err)
+					continue
+				}
+				go norm.SenderThread(node)
+				go norm.ReceiverThread(node)
+			}
+		case "ls":
+			t.PrintTable()
+		case "s":
+			if len(tokens) < 3 {
+				fmt.Println("s usage: s <socketID> <bytes>")
+				continue
+			}
+			sock, err := strconv.ParseUint(tokens[1], 10, 16)
+			if err != nil {
+				fmt.Println("Could not parse socket ID as int")
+				continue
+			}
+			// check socket ID is valid
+			sk, ok := t.SID_to_sk[uint16(sock)]
+			if !ok {
+				fmt.Println("Could not find socket")
+				continue
+			}
+			socket, ok := t.SocketTable[sk]
+			if !ok {
+				fmt.Println("Could not find socket")
+				continue
+			}
+			message := strings.Join(tokens[2:], " ")
+			socket.(*tcpstack.NormalSocket).VWrite([]byte(message))
+		case "r":
+			if len(tokens) != 3 {
+				fmt.Println("r usage: r <socket ID> <numbytes>")
+				continue
+			}
+			sock, err := strconv.ParseUint(tokens[1], 10, 16)
+			if err != nil {
+				fmt.Println("Could not parse socket ID as int")
+				continue
+			}
+			num, err := strconv.ParseUint(tokens[2], 10, 16)
+			if err != nil {
+				fmt.Println("Could not parse numbytes as int")
+				continue
+			}
+			// check socket ID is valid
+			sk, ok := t.SID_to_sk[uint16(sock)]
+			if !ok {
+				fmt.Println("Could not find socket")
+				continue
+			}
+
+			socket, ok := t.SocketTable[sk]
+			if !ok {
+				fmt.Println("Could not find socket")
+				continue
+			}
+			socket.(*tcpstack.NormalSocket).VRead(uint16(num))
+
+		case "cl":
+			fmt.Println("to do")
+		case "sf":
+			fmt.Println("to do")
+		case "rf":
+			fmt.Println("to do")
+		default:
+
+		}
+		fmt.Print("> ")
+	}
+}
+
+func cleanInput(text string) string {
+	output := strings.TrimSpace(text)
+	output = strings.ToLower(output)
+	return output
+}
